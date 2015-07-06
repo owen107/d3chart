ff34473f1e6ecfadaacea0d0669a8a5a1a05fbd4
owen107 <owenxu107@gmail.com>
2015-07-06 09:36:33 -0700

    Finished basic version of stacked bar

--- app.js
+++ app.js
@@ -62,12 +62,16 @@ app.directive('stackedBar', ['$window', '$timeout', 'd3Service', function($windo
 	          var margin = {top: 55, right: 25, bottom: 15, left: 125},
 		          width = 800 - margin.left - margin.right,
 			      height = 550 - margin.top - margin.bottom,
-			      headers = ['Low Activity', 'Med Activity', 'High Activity'];
+			      headers = ['Low Activity', 'Med Activity', 'High Activity'],
+			      tempColor;
 
 			  var yScale = d3.scale.ordinal()
 			      .rangeRoundBands([0, height], .66, 0.2);
   
 			  var xScale = d3.scale.linear()
+			      .rangeRound([0, width]);
+
+			  var xScale2 = d3.scale.linear()
 			      .rangeRound([0, width]);
   
 			  var colors = d3.scale.ordinal()
@@ -79,6 +83,10 @@ app.directive('stackedBar', ['$window', '$timeout', 'd3Service', function($windo
 			      .tickSize(-height)
                   .tickPadding(8)
 			      .orient("top");
+
+			  var xAxis2 = d3.svg.axis()
+			      .scale(xScale2)
+			      .orient('top')
   
 			  var yAxis = d3.svg.axis()
 			      .scale(yScale)
@@ -92,6 +100,14 @@ app.directive('stackedBar', ['$window', '$timeout', 'd3Service', function($windo
                   .attr('id', 'stacked-bar')
                   .append('g')
                   .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+
+              var toolTip = d3.select('body').append('div')
+		          .style('position', 'absolute')
+		          .style('padding', '10px 15px')
+		          .style('border-radius', '5px')
+		          .style('background', '#ccc')
+		          .style('fill', '#000')
+		          .style('opacity', 0);
 			      
 		      var layers = d3.layout.stack()(['low', 'med', 'high'].map(function(level) {
 		      	   return scope.data.map(function(d) {
@@ -102,11 +118,13 @@ app.directive('stackedBar', ['$window', '$timeout', 'd3Service', function($windo
 		      	   }); 
 		      }));
 
-		      console.log(layers);
-
 		      // set up the x-domain and y-domain
 		      xScale.domain([0, d3.max(layers[layers.length - 1], function(d) { 
 			  	    return d.y0 + d.y;
+			  })]);
+
+			  xScale2.domain([0, d3.max(layers[layers.length - 1], function(d) { 
+			  	    return (d.y0 + d.y) / 10;
 			  })]);
 
 			  yScale.domain(layers[0].map(function(d) {
@@ -127,25 +145,65 @@ app.directive('stackedBar', ['$window', '$timeout', 'd3Service', function($windo
 		          	return d; 
 		          })
 		          .enter().append("rect")
-		          .attr("x", function(d) {
-		          	  console.log(d);
-		          	  return xScale(d.y0);
-		          })
+		          .attr("x", 0)
+		          .attr("width", 0)
 		          .attr("y", function(d) { 
 		          	  return yScale(d.x); 
 		          })
 		          .attr("height", yScale.rangeBand())
-		          .attr("width", function(d) {
-                       return xScale(d.y);
-		          })
-		          .on("click", function(d) {
-		               console.log(d.y);
-		          });
+		          .on('mouseover', function(d) {
+                  
+				    toolTip.transition()
+				        .duration(500)
+				        .style('opacity', 1)
+
+				    toolTip.html(d.y)
+				        .style('left', (d3.event.pageX) + 'px')
+				        .style('top', (d3.event.pageY) + 'px')
+
+				    tempColor = this.style.fill;
+				    d3.select(this)
+				        .style('opacity', 0.8)
+				  })
+				  .on('mouseleave', function(d) {
+				  	  toolTip.style('opacity', 0);
+				      d3.select(this)
+				         .style('opacity', 1)
+				         .style('fill', tempColor)
+				  });
+
+		      rects.transition()
+		          .duration(2000)
+		          .ease('elastic')
+			      .delay(function(d, i) { 
+			      	  return i * 30; 
+			      })
+			      .attr("x", function(d) {
+			          return xScale(d.y0); 
+			      })
+			      .attr("width", function(d) { 
+			      	  return xScale(d.y); 
+			      });
 	          
 	          var hAxis = svg.append('g')
 	               .attr('class', 'x-axis')
 	               .attr('transform', 'translate(0, 0)')
 	               .call(xAxis);
+
+	          hAxis.append('text')
+	               .style('font-size', '12px')
+	               .attr('transform', 'translate('+ (width - 43) + ', -28)')
+	               .text('# of Members');
+
+	          var hAxis2 = svg.append('g')
+	               .attr('class', 'x-axis2')
+	               .attr('transform', 'translate(0, ' + (height-40) + ')')
+	               .call(xAxis2);
+
+	          hAxis.append('text')
+	               .style('font-size', '12px')
+	               .attr('transform', 'translate('+ (width + 15) + ', ' + (height - 50) + ')')
+	               .text('%');
 
 	          var vAxis = svg.append('g')
 	               .attr('class', 'y-axis')
@@ -154,13 +212,20 @@ app.directive('stackedBar', ['$window', '$timeout', 'd3Service', function($windo
 	          hAxis.selectAll("path")
 			     .style({"fill": "none", "stroke": "none"});
 
+			  hAxis2.selectAll("path")
+			     .style({"fill": "none", "stroke": "none"});
+
 		      hAxis.selectAll("line")
 		         .style("fill", "none")
 		         .style("stroke", "#000")
 		         .style('stroke-opacity', 0.15)
 		         .style("shape-rendering", "crispEdges");
 			       
-		      hAxis.selectAll('text')
+		      hAxis.selectAll('.tick text')
+		         .style('font-size', '13px')
+		         .style('fill', '#c2c2c2');
+
+		      hAxis2.selectAll('.tick text')
 		         .style('font-size', '13px')
 		         .style('fill', '#c2c2c2');
 
@@ -204,7 +269,7 @@ app.directive('stackedBar', ['$window', '$timeout', 'd3Service', function($windo
 			      .text(function(d) { 
 			      	 return d; 
 			      });
-
+			    
 			});
 	    }
 	};
